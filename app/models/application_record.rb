d9f29efe785e7b5fc6998f08e34afb8c3f25d305
wanyamaman <will.i.yeezy@gmail.com>
2017-04-06 23:23:33 +0300

    Add contact page

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,12 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'capybara-webkit'
+Capybara::Webkit.configure do |capy_config|
+    capy_config.block_unknown_urls
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
